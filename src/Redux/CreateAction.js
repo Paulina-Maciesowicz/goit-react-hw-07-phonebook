efce198e63217f279a88a450dab6dfa475e89a77
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-07-22 16:01:16 +0200

    update
    update

---
+++ src/Redux/CreateAction.js
@@ -0,0 +1,18 @@
+import { createAction, nanoid } from '@reduxjs/toolkit';
+
+export const addContact = createAction(
+  'contacts/addContact',
+  ({ name, number }) => {
+    return {
+      payload: {
+        name,
+        number,
+        id: nanoid(),
+      },
+    };
+  }
+);
+
+export const removeContact = createAction('contacts/removeContact');
+
+export const setFilter = createAction('contacts/setFilter');
